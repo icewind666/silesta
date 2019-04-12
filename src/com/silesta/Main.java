b5f8b3c95f0347ce707a2da8c816d0697a81c82b
Vladimir <Korennoy>
2019-04-12 11:01:31 +0300

    neuro

--- src/com/silesta/Main.java
+++ src/com/silesta/Main.java
@@ -11,31 +11,45 @@ import com.silesta.util.Command;
 import com.silesta.voice.SilestaVoice;
 import marytts.exceptions.MaryConfigurationException;
 import marytts.exceptions.SynthesisException;
-import sun.util.logging.PlatformLogger;
+//import sun.util.logging.PlatformLogger;
+
+import org.neuroph.contrib.graphml.ExampleNetworXOR;
+import org.neuroph.contrib.graphml.GraphmlExport;
+import org.neuroph.core.NeuralNetwork;
+
 
 public class Main {
     private static Logger log = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) {
         log.info("S.I.L.E.S.T.A project is starting up, ...");
-        SilestaVoice voice = new SilestaVoice();
 
-        if (args.length >= 1) {
-            //user specified a config file
-            Properties p = new Properties();
+        NeuralNetwork ann = ExampleNetworXOR.getNetwork();
+        
+        GraphmlExport ge = new GraphmlExport( ann );
+        ge.parse();
+        ge.printToStdout();
 
-            try {
-                p.load(new FileInputStream(args[0]));
-                String commandToRun = args[1];
-                CommandRunner runner = new CommandRunner(commandToRun, p);
-                runner.go();
 
-            } catch (IOException e) {
-                e.printStackTrace();
-                log.severe("Error: I cant load properties file");
-                log.log(Level.SEVERE, "Exception: ", e);
-            }
 
-        }
+//        SilestaVoice voice = new SilestaVoice();
+//
+//        if (args.length >= 1) {
+//            //user specified a config file
+//            Properties p = new Properties();
+//
+//            try {
+//                p.load(new FileInputStream(args[0]));
+//                String commandToRun = args[1];
+//                CommandRunner runner = new CommandRunner(commandToRun, p);
+//                runner.go();
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                log.severe("Error: I cant load properties file");
+//                log.log(Level.SEVERE, "Exception: ", e);
+//            }
+//
+//        }
     }
 }
