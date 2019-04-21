c5b05cb2b8431d371ebd79151e37b407fe224462
korennoy-va <korennoy-va@rcbi.company>
2019-04-21 13:34:34 +0300

    u

--- src/com/silesta/Main.java
+++ src/com/silesta/Main.java
@@ -27,6 +27,8 @@ import org.neuroph.nnet.MultiLayerPerceptron;
 import org.neuroph.nnet.Perceptron;
 import org.neuroph.nnet.learning.PerceptronLearning;
 import org.neuroph.nnet.learning.ResilientPropagation;
+import org.neuroph.util.TransferFunctionType;
+import org.neuroph.util.data.norm.MaxMinNormalizer;
 import org.neuroph.util.data.norm.ZeroMeanNormalizer;
 
 
@@ -35,83 +37,65 @@ public class Main {
 
     public static void main(String[] args) {
         log.info("S.I.L.E.S.T.A project is starting up, ...");
+        boolean RECALCULATE = true;
 
-// create new perceptron network
-        NeuralNetwork neuralNetwork = new Perceptron(3,1);
-        neuralNetwork.setLearningRule(new ResilientPropagation());
-// create training set
-        DataSet trainingSet;
-// add training data to training set (logical OR function)
 
-//        trainingSet.addRow(new DataSetRow (new double[]{0, 0},
-//                new double[]{0}));
-//        trainingSet.addRow(new DataSetRow (new double[]{0, 1},
-//                new double[]{1}));
-//        trainingSet.addRow(new DataSetRow (new double[]{1, 0},
-//                new double[]{1}));
-//        trainingSet.addRow(new DataSetRow(new double[]{1, 1},
-//                new double[]{1}));
 
-        trainingSet = DataSet.createFromFile("data.csv",3,1, ",", true);
-        // learn the training set
-        ZeroMeanNormalizer norm = new ZeroMeanNormalizer();
-        norm.normalize(trainingSet);
+        if(RECALCULATE) {
 
-        //NeuralNetworkEventListener listener = neuralNetworkEvent -> System.out.println(neuralNetworkEvent.getEventType().name());
-        ResilientPropagation rule = (ResilientPropagation)neuralNetwork.getLearningRule();
-        rule.setMaxError(0.22);
-        Runnable runnable =
-                () -> { neuralNetwork.learn(trainingSet); };
-        Thread t = new Thread(runnable);
-        t.start();
-//        neuralNetwork.addListener(listener);
-//        neuralNetwork.learn(trainingSet);
+            NeuralNetwork neuralNetwork = new MultiLayerPerceptron(TransferFunctionType.SIGMOID,3, 50, 1);
+            neuralNetwork.setLearningRule(new ResilientPropagation());
+            DataSet trainingSet;
+            trainingSet = DataSet.createFromFile("data.csv", 3, 1, ",", true);
 
-        while(!rule.isStopped()) {
-            if(rule.getCurrentIteration() % 100000 == 0) {
+            // norm = new ZeroMeanNormalizer();
+            MaxMinNormalizer norm = new MaxMinNormalizer();
+            norm.normalize(trainingSet);
+
+            ResilientPropagation rule = (ResilientPropagation) neuralNetwork.getLearningRule();
+            //rule.setMaxIterations(10000000);
+            rule.setMaxError(0.05);
+
+            Runnable runnable = () -> neuralNetwork.learn(trainingSet);
+            Thread t = new Thread(runnable);
+            t.start();
+
+            while (!rule.isStopped()) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.printf("iteration: %s\n", rule.getCurrentIteration());
                 System.out.printf("Prev error : %s\n", rule.getPreviousEpochError());
                 System.out.printf("Overall error: %s\n", rule.getTotalNetworkError());
             }
+
+            // save the trained network into file
+            neuralNetwork.save("net.nnet");
+            GraphmlExport ge = new GraphmlExport(neuralNetwork);
+            ge.parse();
+            ge.writeToFile("graphml.xml");
+
         }
 
-        // save the trained network into file
-        neuralNetwork.save("net.nnet");
-        GraphmlExport ge = new GraphmlExport( neuralNetwork );
-        ge.parse();
-        ge.writeToFile("graphml.xml");
-
-
         // load the saved network
-//        NeuralNetwork neuralNetworkDone = NeuralNetwork.createFromFile("or_perceptron.nnet");
-//        // set network input
-//        neuralNetworkDone.setInput(0, 1, 2);
-//        // calculate network
-//        neuralNetworkDone.calculate();
-//        // get network output
-//        double[] networkOutput = neuralNetworkDone.getOutput();
-//
-//        System.out.printf("%s", Arrays.toString(networkOutput));
-//
-//
-//        SilestaVoice voice = new SilestaVoice();
-//
-//        if (args.length >= 1) {
-//            //user specified a config file
-//            Properties p = new Properties();
-//
-//            try {
-//                p.load(new FileInputStream(args[0]));
-//                String commandToRun = args[1];
-//                CommandRunner runner = new CommandRunner(commandToRun, p);
-//                runner.go();
-//
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                log.severe("Error: I cant load properties file");
-//                log.log(Level.SEVERE, "Exception: ", e);
-//            }
-//
-//        }
+        NeuralNetwork neuralNetworkDone = NeuralNetwork.createFromFile("net.nnet");
+        // set network input
+        neuralNetworkDone.setInput(0, 1, 3);
+        neuralNetworkDone.calculate();
+        double[] networkOutput = neuralNetworkDone.getOutput();
+        System.out.printf("%s", Arrays.toString(networkOutput));
+
+        neuralNetworkDone.setInput(1, 1, 3);
+        neuralNetworkDone.calculate();
+        double[] networkOutput2 = neuralNetworkDone.getOutput();
+        System.out.printf("%s", Arrays.toString(networkOutput2));
+
+        neuralNetworkDone.setInput(1, 2, 3);
+        neuralNetworkDone.calculate();
+        double[] networkOutput3 = neuralNetworkDone.getOutput();
+        System.out.printf("%s", Arrays.toString(networkOutput3));
+
     }
 }
